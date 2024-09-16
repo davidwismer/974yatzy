ac6c2b3e243d45fa8e83fa812fbbe9bc8eb0f5a7
David Wismer <david.wismer@mindmaze.ch>
2024-09-16 19:53:16 +0200

    Prevent page unload

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App).mount('#app')
+
+window.addEventListener("beforeunload", (event) => {
+    event.preventDefault()
+})
